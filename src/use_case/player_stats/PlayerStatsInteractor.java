a06d7eae1712c1a945e005b0f5fe6291cc124a77
mr-adamo <129005516+mr-adamo@users.noreply.github.com>
2023-11-06 21:38:58 -0500

    Worked on PlayerStatsInteractor, PlayerStatsOutputData

--- src/use_case/player_stats/PlayerStatsInteractor.java
+++ src/use_case/player_stats/PlayerStatsInteractor.java
@@ -1,4 +1,31 @@
 package use_case.player_stats;
 
-public class PlayerStatsInteractor {
+import interface_adapter.player_stats.PlayerStatsPresenter;
+
+import java.util.Dictionary;
+import java.util.Objects;
+
+public class PlayerStatsInteractor implements PlayerStatsInputBoundary {
+    final PlayerStatsDataAccessInterface playerStatsAccessObject;
+    final PlayerStatsOutputData playerStatsPresenter;
+
+    public PlayerStatsInteractor(PlayerStatsDataAccessInterface playerStatsAccessObject, PlayerStatsOutputData playerStatsOutputBoundary) {
+        this.playerStatsAccessObject = playerStatsAccessObject;
+        this.playerStatsPresenter = playerStatsOutputBoundary;
+    }
+
+    @Override
+    public void execute(PlayerStatsInputData inputData) {
+        int id = inputData.getId();
+        if (!playerStatsAccessObject.existsById(id)) {
+            PlayerStatsPresenter.prepareFailView("Player ID does not exist.");
+        } else {
+            try {
+                Dictionary<String, Object> playerStats = playerStatsAccessObject.getStats();
+                playerStatsPresenter.prepareSuccessView(new PlayerStatsOutputData(playerStats, true));
+            } catch (Exception e) {
+                playerStatsPresenter.prepareFailView(e.toString());
+            }
+        }
+    }
 }
