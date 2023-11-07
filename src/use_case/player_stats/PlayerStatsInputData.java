1edcde3fdde16b4cda47c023156b78535896d95b
jungurilee <junguri.lee@mail.utoronto.ca>
2023-11-06 21:15:48 -0500

    Finished Input Data for Player Stats

--- src/use_case/player_stats/PlayerStatsInputData.java
+++ src/use_case/player_stats/PlayerStatsInputData.java
@@ -1,4 +1,12 @@
 package use_case.player_stats;
 
 public class PlayerStatsInputData {
+
+    final private int id;
+
+    public PlayerStatsInputData(int id) {
+        this.id = id;
+    }
+
+    int getId() {return id;}
 }
