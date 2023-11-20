4dd9c3cdc447a360675a7fe45987eeaf6dc1bee2
pingshan <tony.pingshan@mail.utoronto.ca>
2023-11-20 03:19:52 -0500

    Updated Input Boundary for Team Stats, interactor still not fully implemented

--- src/use_case/team_stats/TeamStatsInputBoundary.java
+++ src/use_case/team_stats/TeamStatsInputBoundary.java
@@ -1,5 +1,5 @@
 package use_case.team_stats;
 
 public interface TeamStatsInputBoundary {
-    void execute(TeamStatsInputBoundary teamStatsInputBoundary);
+    TeamStatsOutputData getTeamStats(int teamId);
 }
