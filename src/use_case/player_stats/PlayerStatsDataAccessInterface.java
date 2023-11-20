1800992e952451ce6fcc7b321d7074ecef0482cb
Noah Black <64273297+NoahBlack012@users.noreply.github.com>
2023-11-20 12:04:03 -0500

    Data access interface and existsById method

--- src/use_case/player_stats/PlayerStatsDataAccessInterface.java
+++ src/use_case/player_stats/PlayerStatsDataAccessInterface.java
@@ -1,4 +1,11 @@
 package use_case.player_stats;
 
+import entity.Player;
+
+import java.util.Map;
+
 public interface PlayerStatsDataAccessInterface {
+    boolean existsById(int playerId);
+    Player getPlayerInfo(int playerID);
+    Map<String, Object> getPlayerYearlyStats(int playerID, int season);
 }
