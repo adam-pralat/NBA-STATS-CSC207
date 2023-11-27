d2295963f90b4afe25025066936e668417955e11
pingshan <tony.pingshan@mail.utoronto.ca>
2023-11-27 18:06:34 -0500

    finished implementing TeamStatsOutputData

--- src/use_case/team_stats/TeamStatsOutputData.java
+++ src/use_case/team_stats/TeamStatsOutputData.java
@@ -1,4 +1,19 @@
 package use_case.team_stats;
 
+import java.util.Hashtable;
+import java.util.Map;
+
 public class TeamStatsOutputData {
+
+    private Map<String, Object> teamStats = new Hashtable<>();
+    private boolean useCaseFailed;
+
+    public TeamStatsOutputData(Map<String, Object> teamStats, boolean useCaseFailed) {
+        this.teamStats = teamStats;
+        this.useCaseFailed = useCaseFailed;
+    }
+
+    public Map<String, Object> getTeamStats() {
+        return this.teamStats;
+    }
 }
