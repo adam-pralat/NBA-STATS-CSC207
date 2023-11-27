ace4d02e03176ef7407fcb5e058fbb423032dde4
pingshan <tony.pingshan@mail.utoronto.ca>
2023-11-27 17:36:37 -0500

    Finished Team Stats DataAccessInterface

--- src/use_case/team_stats/TeamStatsDataAccessInterface.java
+++ src/use_case/team_stats/TeamStatsDataAccessInterface.java
@@ -1,4 +1,13 @@
 package use_case.team_stats;
 
+import entity.Team;
+import org.json.JSONException;
+
+import java.util.Map;
+
 public interface TeamStatsDataAccessInterface {
+    public boolean existsById(int teamID) throws JSONException;
+    Team getTeamInfo(int teamID);
+    Map<String, Object> getYearlyRecord(int teamID, int season);
+    Map<String, Object> getTeamYearlyStats(int teamID, int season);
 }
