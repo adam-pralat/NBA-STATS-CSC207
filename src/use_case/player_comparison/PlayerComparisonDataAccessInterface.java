0668d39033bacfa93dab7c84bebb56db93c1c74b
RMagill46 <ross.magill@mail.utoronto.ca>
2023-11-29 09:58:07 -0500

    PlayerComparisonDataAccessInterface.java, PlayerComparisonInputData.java, PlayerComparisonInteractor.java

--- src/use_case/player_comparison/PlayerComparisonDataAccessInterface.java
+++ src/use_case/player_comparison/PlayerComparisonDataAccessInterface.java
@@ -3,7 +3,10 @@ package use_case.player_comparison;
 import entity.Player;
 import org.json.JSONException;
 
+import java.util.Map;
+
 public interface PlayerComparisonDataAccessInterface {
     public boolean existsById(int playerID) throws JSONException;
-    //TODO
+    Player getPlayerInfo(int playerID);
+    Map<String, Object> getPlayerYearlyStats(int playerID, int season);
 }
--- src/use_case/player_comparison/PlayerComparisonInputData.java
+++ src/use_case/player_comparison/PlayerComparisonInputData.java
@@ -5,22 +5,18 @@ import java.util.ArrayList;
 public class PlayerComparisonInputData {
     final private int id1;
     final private int id2;
-    final private int season;
 
-    public PlayerComparisonInputData(int id1, int id2, int season) {
+    public PlayerComparisonInputData(int id1, int id2) {
         if (id1 <= 0 || id2 <= 0) {
             throw new IllegalArgumentException("id cant be negative.");
         }
         this.id1 = id1;
         this.id2 = id2;
-        this.season = season;
     }
 
     int getId1() {return id1;}
 
     int getId2() {return id2;}
-
-    int getSeason() {return season;}
 
     ArrayList<Integer> getBothId() {
         ArrayList<Integer> Ids = new ArrayList<>();
