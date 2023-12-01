40de24d8098163e030d5e626e5c3b7954e5d4d09
RMagill46 <ross.magill@mail.utoronto.ca>
2023-12-01 10:43:44 -0500

    Added and implemented IdInformationUseCaseFactory.java, made changes to PlayerComparisonUseCaseFactory.java

--- src/app/PlayerComparisonUseCaseFactory.java
+++ src/app/PlayerComparisonUseCaseFactory.java
@@ -1,26 +1,44 @@
 package app;
 
 import interface_adapter.ViewManagerModel;
+import interface_adapter.id_information.IdInformationController;
+import interface_adapter.id_information.IdInformationViewModel;
 import interface_adapter.player_comparison.PlayerComparisonController;
 import interface_adapter.player_comparison.PlayerComparisonPresenter;
 import interface_adapter.player_comparison.PlayerComparisonViewModel;
+import use_case.id_information.IdInformationDataAccessInterface;
 import use_case.player_comparison.PlayerComparisonDataAccessInterface;
 import use_case.player_comparison.PlayerComparisonInputBoundary;
 import use_case.player_comparison.PlayerComparisonInteractor;
 import use_case.player_comparison.PlayerComparisonOutputBoundary;
 import view.PlayerComparisonView;
 
+import javax.swing.*;
 import java.io.IOException;
+
+import static app.IdInformationUseCaseFactory.getController;
 
 public class PlayerComparisonUseCaseFactory {
     private PlayerComparisonUseCaseFactory() {}
 
     public static PlayerComparisonView create(ViewManagerModel viewManagerModel,
                                               PlayerComparisonViewModel playerComparisonViewModel,
-                                              PlayerComparisonDataAccessInterface playerComparisonDAO) throws IOException {
+                                              PlayerComparisonDataAccessInterface playerComparisonDAO,
+                                              IdInformationViewModel idInformationViewModel,
+                                              IdInformationDataAccessInterface idInformationDataAccessInterface)
+            throws IOException {
+        try{
         PlayerComparisonController playerComparisonController = createPlayerComparisonUseCase(viewManagerModel,
                 playerComparisonViewModel, playerComparisonDAO);
-        return new PlayerComparisonView(playerComparisonController, playerComparisonViewModel);
+        IdInformationController idInformationController = getController(idInformationViewModel, idInformationDataAccessInterface);
+        return new PlayerComparisonView(playerComparisonController,
+                playerComparisonViewModel,
+                idInformationController,
+                idInformationViewModel);
+    } catch (IOException e) {
+        JOptionPane.showMessageDialog(null, "Could not open data file.");
+        }
+        return null;
     }
 
     private static PlayerComparisonController createPlayerComparisonUseCase(
