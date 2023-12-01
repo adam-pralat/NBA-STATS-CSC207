18f868b27d1c7a76f44be532896504df22ab43fd
pingshan <tony.pingshan@mail.utoronto.ca>
2023-12-01 15:56:20 -0500

    Finished implementing team stats ViewModel

--- src/interface_adapter/team_stats/TeamStatsViewModel.java
+++ src/interface_adapter/team_stats/TeamStatsViewModel.java
@@ -1,4 +1,32 @@
 package interface_adapter.team_stats;
 
-public class TeamStatsViewModel {
+import entity.Team;
+import interface_adapter.ViewModel;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
+public class TeamStatsViewModel extends ViewModel {
+
+    private TeamStatsState state;
+
+    public TeamStatsViewModel(){super("teamStats");}
+
+    private final PropertyChangeSupport support = new PropertyChangeSupport(this);
+
+    public void firePropertyChanged() {
+        support.firePropertyChange("state", null, this.state);
+    }
+
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        support.addPropertyChangeListener(listener);
+    }
+
+    public TeamStatsState getState() {
+        return state;
+    }
+
+    public void setState(TeamStatsState state) {
+        this.state = state;
+    }
 }
