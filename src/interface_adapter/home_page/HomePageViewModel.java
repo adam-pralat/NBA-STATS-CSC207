b46e288dc355f8ea258ec63519d8b040d0c0eff5
Noah Black <64273297+NoahBlack012@users.noreply.github.com>
2023-12-02 18:28:09 -0500

    Home page view generally completed. Note that the controller is called when the class is instantiated - Maybe do this in a different way

--- src/interface_adapter/home_page/HomePageViewModel.java
+++ src/interface_adapter/home_page/HomePageViewModel.java
@@ -12,7 +12,25 @@ public class HomePageViewModel extends ViewModel {
     public String PLAYER_STATS_BUTTON_LABEL = "View Player Statistics";
     public String TEAM_STATS_BUTTON_LABEL = "View Team Statistics";
     public String PLAYER_COMPARISON_BUTTON_LABEL = "Compare Two Players";
-    public String[] PLAYER_INFO_COLUMN_NAMES = {"A", "B", "C", "D"};
+    public String[] PLAYER_ROW_NAMES = {
+            "Name", "Birth Date", "Country", "Team",
+            "Points Per Game", "Assists Per Game", "Free Throw Percentage",
+            "Field Goal Percentage", "Three Point Percentage", "Minutes Per Game",
+            "Steals Per Game", "Turnovers Per Game", "Blocks Per Game"
+    };
+    public String[] TEAM_ROW_NAMES = {
+            "Team", "Nickname", "Code",
+            "Conference", "Conference Place", "Wins",
+            "Losses", "Wins (Past 10 Games)", "Losses (Past 10 Games)",
+            "Points Per Game", "Free Throw Percentage", "Field Goal Percentage",
+            "Three Point Percentage"
+    };
+    public String PLAYER_HEADER = "Featured Player";
+    public String TEAM_HEADER = "Featured Team";
+    public String PAGE_TITLE = "Java Jumpman - NBA Statistics";
+    public float PAGE_TITLE_FONT_SIZE = 56.0f;
+    public float HEADER_FONT_SIZE = 28.0f;
+
 
     public HomePageViewModel(){ super("home page"); } // TODO: ViewName should be an argument??
     // - Issue with switching views occurs when viewName string is different between view and view model
