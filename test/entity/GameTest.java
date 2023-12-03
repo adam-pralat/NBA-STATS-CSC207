40c503c3ce0534bbd5503225203302b5e71802bb
Adam Pralat <129005516+adam-pralat@users.noreply.github.com>
2023-12-02 21:58:11 -0500

    Update GameTest.java

--- test/entity/GameTest.java
+++ test/entity/GameTest.java
@@ -37,65 +37,56 @@ public class GameTest extends TestCase {
     }
 
     public void testGetHomeTeamWins() {
-        assertEquals((int)game.getHomeTeamWins(), 1);
+        assertEquals(game.getHomeTeamWins(), 1);
     }
 
     public void testSetHomeTeamWins() {
         game.setHomeTeamWins(5);
-        assertEquals((int)game.getHomeTeamWins(), 5);
+        assertEquals(game.getHomeTeamWins(), 5);
     }
 
     public void testGetHomeTeamLosses() {
-        assertEquals((int)game.getHomeTeamLosses(), 2);
+        assertEquals(game.getHomeTeamLosses(), 2);
     }
 
     public void testSetHomeTeamLosses() {
         game.setHomeTeamLosses(10);
-        assertEquals((int)game.getHomeTeamLosses(), 10);
+        assertEquals(game.getHomeTeamLosses(), 10);
     }
 
     public void testGetHomeTeamPoints() {
-        assertEquals((int)game.getHomeTeamPoints(), 2);
+        assertEquals(game.getHomeTeamPoints(), 2);
     }
 
     public void testSetHomeTeamPoints() {
         game.setHomeTeamPoints(4);
-        assertEquals((int)game.getHomeTeamPoints(), 4);
+        assertEquals(game.getHomeTeamPoints(), 4);
     }
 
     public void testGetAwayTeamWins() {
-        assertEquals((int)game.getAwayTeamWins(), 2);
+        assertEquals(game.getAwayTeamWins(), 2);
     }
 
     public void testSetAwayTeamWins() {
         game.setAwayTeamWins(10);
-        assertEquals((int)game.getAwayTeamWins(), 10);
+        assertEquals(game.getAwayTeamWins(), 10);
     }
 
     public void testGetAwayTeamLosses() {
-        assertEquals((int)game.getAwayTeamLosses(), 1);
+        assertEquals(game.getAwayTeamLosses(), 1);
     }
 
     public void testSetAwayTeamLosses() {
         game.setAwayTeamLosses(5);
-        assertEquals((int)game.getAwayTeamLosses(), 5);
+        assertEquals(game.getAwayTeamLosses(), 5);
     }
 
     public void testGetAwayTeamPoints() {
-        assertEquals((int)game.getAwayTeamPoints(), 4);
+        assertEquals(game.getAwayTeamPoints(), 4);
     }
 
     public void testSetAwayTeamPoints() {
         game.setAwayTeamPoints(40);
-        assertEquals((int)game.getAwayTeamPoints(), 40);
-    }
-
-    public void testTestToString() {
-        assertEquals(game.toString(), "\n________________________________________________________\n" +
-                "2023-01-01\n" +
-                "Finished\n" +
-                "Amway Center, Orlando, FL\n" +
-                "ORL | Orlando Magic : 2 - 4 : Toronto Raptors | TOR\n" +
-                "(1 - 2)\t\t(2 - 1)\n");
+        assertEquals(game.getAwayTeamPoints(), 40);
     }
 }
